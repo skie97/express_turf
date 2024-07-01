d5a552dcc3a23940a986eb34eb66d028d3968d16
Jerome Tay <thatsme@jerometay.com>
2024-07-01 13:04:12 -0400

    added readme

--- src/intersectSightings.test.ts
+++ src/intersectSightings.test.ts
@@ -1,5 +1,6 @@
 import { correctBearingToGeoJSON, getSector, getIntersection, sightingData } from './intersectSightings';
 import * as turf from '@turf/turf';
+import { addHours } from 'date-fns';
 
 test('correctBearingToGeoJSON', () => {
     expect(correctBearingToGeoJSON(50)).toBe(50);
@@ -16,8 +17,8 @@ test('correctBearingToGeoJSON', () => {
 test('getIntersection: getSector with single position and no boundary', () => {
     const currentTime: Date = new Date();
     const firstSighting: sightingData = {
-        sightingDateTime: currentTime,
-        avgSpeedInKmh: 0,
+        sightingDateTime: addHours(currentTime, -1),
+        avgSpeedInKmh: 1,
         position: turf.point([0,0,17]),
         bearingInDeg: 90,
         degreeOfUncertainty: 3
@@ -29,6 +30,7 @@ test('getIntersection: getSector with single position and no boundary', () => {
         bearingInDeg: 90,
         degreeOfUncertainty: 3
     }
-    const result = getIntersection([firstSighting, secondSighting], currentTime)
-    // console.log(result.geometry.coordinates);
+    const result = getIntersection([firstSighting], currentTime)
+    console.log(result.geometry.coordinates);
+    console.log(getIntersection([secondSighting], currentTime).geometry.coordinates);
 })
