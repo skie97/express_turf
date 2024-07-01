22829a9209b2bdd5817ed4f803fb483a66d6cbd9
Jerome Tay <thatsme@jerometay.com>
2024-07-01 12:25:07 -0400

    intersect functions complete

--- src/intersectSightings.test.ts
+++ src/intersectSightings.test.ts
@@ -1,4 +1,5 @@
-import { correctBearingToGeoJSON } from './intersectSightings';
+import { correctBearingToGeoJSON, getSector, getIntersection, sightingData } from './intersectSightings';
+import * as turf from '@turf/turf';
 
 test('correctBearingToGeoJSON', () => {
     expect(correctBearingToGeoJSON(50)).toBe(50);
@@ -8,4 +9,26 @@ test('correctBearingToGeoJSON', () => {
     expect(correctBearingToGeoJSON(-10)).toBe(-10);
     expect(correctBearingToGeoJSON(-200)).toBe(160);
     expect(correctBearingToGeoJSON(-370)).toBe(-10);
+});
+
+// TODO: to test with excel values.
+// It's easier to run tests by running through getIntersection and passing various arrays
+test('getIntersection: getSector with single position and no boundary', () => {
+    const currentTime: Date = new Date();
+    const firstSighting: sightingData = {
+        sightingDateTime: currentTime,
+        avgSpeedInKmh: 0,
+        position: turf.point([0,0,17]),
+        bearingInDeg: 90,
+        degreeOfUncertainty: 3
+    }
+    const secondSighting: sightingData = {
+        sightingDateTime: currentTime,
+        avgSpeedInKmh: 0,
+        position: turf.point([0,0,17]),
+        bearingInDeg: 90,
+        degreeOfUncertainty: 3
+    }
+    const result = getIntersection([firstSighting, secondSighting], currentTime)
+    // console.log(result.geometry.coordinates);
 })
--- src/intersectSightings.ts
+++ src/intersectSightings.ts
@@ -1,5 +1,14 @@
-import turf from '@turf/turf';
+import * as turf from '@turf/turf';
 import { Feature, Polygon, Point, MultiPolygon } from 'geojson';
+import { differenceInSeconds } from 'date-fns';
+
+export interface sightingData {
+    position: Feature<Point>,
+    bearingInDeg: number,
+    sightingDateTime: Date,
+    avgSpeedInKmh: number,
+    degreeOfUncertainty: number
+}
 
 export function lineOfSightKm(elevationInMeters: number): number {
     return Math.sqrt(elevationInMeters) * 3.57
@@ -14,7 +23,21 @@ export function correctBearingToGeoJSON(bearingInDeg: number) : number {
     return corrected;
 }
 
-function getPolyCone(position: Feature<Point>, bearingInDeg: number, degreeOfUncertainty: number): Feature<Polygon> {
+export function getSector(position: Feature<Point>, bearingInDeg: number, degreeOfUncertainty: number): Feature<Polygon> {
+    var altitudeInMeters = position.geometry.coordinates[2] || 1.7; // If no elevation, assume it to be a human being of 1.7 meters
+    var losDistanceKm = lineOfSightKm(altitudeInMeters);
+    return turf.sector(position,
+                       losDistanceKm,
+                       correctBearingToGeoJSON(bearingInDeg - degreeOfUncertainty),
+                       correctBearingToGeoJSON(bearingInDeg + degreeOfUncertainty),
+                       {"units": "kilometers"}
+    );
+}
+
+// Limitation: This function doesn't draw a segment, but a triangle. This means that as the degree of Uncertainity increases, it
+// becomes more and more inaccurate. This is included as a first implementation, but the function getSector returns a sector
+// of a circle as expected and should be used instead.
+export function getPolyCone(position: Feature<Point>, bearingInDeg: number, degreeOfUncertainty: number): Feature<Polygon> {
     var altitudeInMeters = position.geometry.coordinates[2] || 1.7; // If no elevation, assume it to be a human being of 1.7 meters
     var losDistanceKm = lineOfSightKm(altitudeInMeters);
     var leftEndPoint = turf.destination(position, 
@@ -39,8 +62,29 @@ function getPolyCone(position: Feature<Point>, bearingInDeg: number, degreeOfUnc
     )
 }
 
-function getPolyWithTimeLapsed(poly: Feature<Polygon>, avgSpeedInKmh: number, timeInHours: number): Feature<Polygon|MultiPolygon> {
-    return turf.buffer(poly, avgSpeedInKmh * timeInHours);
+function getPolyWithTimeLapsed(poly: Feature<Polygon>, avgSpeedInKmh: number, timeInSecs: number): Feature<Polygon|MultiPolygon> {
+    return turf.buffer(poly, avgSpeedInKmh * timeInSecs / (60.0 * 60.0));
 }
 
-module.exports = { correctBearingToGeoJSON, lineOfSightKm };
+export function getIntersection(data: sightingData[], currentTime: Date): Feature<Polygon|MultiPolygon> {
+    if (data.length === 0) {
+        return null;
+    }
+    var result = data.map<Feature<Polygon|MultiPolygon>>(
+        (value) => {
+            return getPolyWithTimeLapsed(
+                getSector(value.position, value.bearingInDeg, value.degreeOfUncertainty),
+                value.avgSpeedInKmh,
+                differenceInSeconds(value.sightingDateTime, currentTime)
+            );
+        }
+    )
+    if (result.length > 1) {
+        return turf.intersect(turf.featureCollection(result));
+    } else {
+        return result[0];
+    }
+}
+
+
+// module.exports = { correctBearingToGeoJSON, lineOfSightKm, getIntersection };
